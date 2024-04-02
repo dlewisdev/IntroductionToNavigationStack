173cd279d4b299d544d06d5d424d1ccd708afb5f
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-04-02 14:20:08 -0400

    - add DrinkDetailView - reorganize project folder structure

---
+++ Introduction to NavigationStack/Views/DrinkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkDetailView.swift
+//  Introduction to NavigationStack
+//
+//  Created by Danielle Lewis on 4/2/24.
+//
+
+import SwiftUI
+
+struct DrinkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrinkDetailView()
+}
