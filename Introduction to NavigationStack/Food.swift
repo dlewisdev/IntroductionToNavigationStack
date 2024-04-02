bd35c51c45f1bc0e4d268143fbe814b0167b547e
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-04-02 13:50:26 -0400

    - add drink model

--- Introduction to NavigationStack/Food.swift
+++ Introduction to NavigationStack/Food.swift
@@ -45,3 +45,18 @@ let foods: [Food] = [
          description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
          price: 12.99)
 ]
+
+struct Drink: Identifiable, Hashable {
+    var id: String { "\(name)_\(title)" }
+    let name: String
+    let title: String
+    let description: String
+    let isFizzy: Bool
+    let price: Decimal
+}
+
+let drinks: [Drink] = [
+    Drink(name: "🥤", title: "Soda", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", isFizzy: true, price: 2.99),
+    Drink(name: "🧋", title: "Boba Tea", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", isFizzy: false, price: 3.99),
+    Drink(name: "🧃", title: "Juice", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", isFizzy: true, price: 0.99)
+]
