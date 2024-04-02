99e0ddb56465fd2acd1e8423f0cf30cb917235a8
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-04-02 14:37:43 -0400

    - update drinks array

--- Introduction to NavigationStack/Models/Food.swift
+++ Introduction to NavigationStack/Models/Food.swift
@@ -65,7 +65,7 @@ struct Drink: MenuItem, Identifiable, Hashable {
 let drinks: [Drink] = [
     Drink(name: "🥤", title: "Soda", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", isFizzy: true, price: 2.99),
     Drink(name: "🧋", title: "Boba Tea", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", isFizzy: false, price: 3.99),
-    Drink(name: "🧃", title: "Juice", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", isFizzy: true, price: 0.99)
+    Drink(name: "🧃", title: "Juice", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", isFizzy: false, price: 0.99)
 ]
 
 struct Dessert: MenuItem, Identifiable, Hashable {
